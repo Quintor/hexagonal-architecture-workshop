ee29379239a4f3ab750ec34c87968bcd1cc9da4c
Jef Koldenhof <jkoldenhof@quintor.nl>
2026-01-29 13:06:58 +0100

    Opzet van arch unit tests voor layer naamgeving validatie

---
+++ src/test/java/nl/quintor/workshop/ArchUnitLayerNamingTest.java
@@ -0,0 +1,58 @@
+package nl.quintor.workshop;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.Test;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+public class ArchUnitLayerNamingTest {
+
+    @Test
+    public void service_classes_should_have_suffix() {
+        JavaClasses importedClasses = new ClassFileImporter().importPackages("nl.quintor.workshop");
+
+        ArchRule rule = classes()
+                .that()
+                .resideInAPackage("..service..")
+                .and()
+                .areAnnotatedWith(Service.class)
+                .should()
+                .haveSimpleNameEndingWith("Service");
+
+        rule.check(importedClasses);
+    }
+
+    @Test
+    public void repository_classes_should_have_suffix() {
+        JavaClasses importedClasses = new ClassFileImporter().importPackages("nl.quintor.workshop");
+
+        ArchRule rule = classes()
+                .that()
+                .resideInAPackage("..repository..")
+                .and()
+                .haveSimpleNameNotContaining("package-info")
+                .should()
+                .haveSimpleNameEndingWith("Repository");
+
+        rule.check(importedClasses);
+    }
+
+    @Test
+    public void controller_classes_should_have_suffix() {
+        JavaClasses importedClasses = new ClassFileImporter().importPackages("nl.quintor.workshop");
+
+        ArchRule rule = classes()
+                .that()
+                .resideInAPackage("..controller..")
+                .and()
+                .areAnnotatedWith(RestController.class)
+                .should()
+                .haveSimpleNameEndingWith("Controller");
+
+        rule.check(importedClasses);
+    }
+}
