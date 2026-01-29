eee172b3bd6d7d04cec4ae82c4001422f2a5c143
Jef Koldenhof <jkoldenhof@quintor.nl>
2026-01-29 10:00:03 +0100

    Taxibedrijf naam toegevoegd

--- src/main/java/nl/quintor/workshop/DemoApplication.java
+++ src/main/java/nl/quintor/workshop/DemoApplication.java
@@ -3,7 +3,9 @@ package nl.quintor.workshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.modulith.Modulith;
 
+@Modulith(systemName = "Taxibedrijf Rebü")
 @SpringBootApplication
 @EnableJpaRepositories
 public class DemoApplication {
