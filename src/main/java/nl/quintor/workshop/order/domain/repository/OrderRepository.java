7d0fd551c668af5649bf83ff3d02bcdbc4890b6e
marcel <mhoekstra@quintor.nl>
2026-02-06 16:10:01 +0100

    uitleg hexagonal lagen verwijderen van Order module

--- src/main/java/nl/quintor/workshop/order/domain/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package nl.quintor.workshop.order.domain.repository;
-
-import nl.quintor.workshop.order.domain.model.Order;
-
-import java.util.List;
-
-public interface OrderRepository {
-    List<Order> findOrders();
-}
--- src/main/java/nl/quintor/workshop/order/infrastructure/adapters/inbound/dto/OrderDto.java
+++
@@ -1,4 +0,0 @@
-package nl.quintor.workshop.order.infrastructure.adapters.inbound.dto;
-
-public record OrderDto (String omschrijving) {
-}
