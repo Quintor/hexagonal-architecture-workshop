40540c72ad6826d9379f09a94c2b6ae691b52587
Jef Koldenhof <jkoldenhof@quintor.nl>
2026-02-20 11:01:37 +0100

    feat: remove C4 style modultih diagrams to simplify onboarding

--- src/test/java/nl/quintor/workshop/ModulithTest.java
+++ src/test/java/nl/quintor/workshop/ModulithTest.java
@@ -7,25 +7,19 @@ import org.springframework.modulith.docs.Documenter;
 class ModulithTest {
     private final ApplicationModules modules = ApplicationModules.of(DemoApplication.class);
 
-    // TODO: ook in ArchUnit doen?
     @Test
     void verifyModules() {
 
         modules.verify();
     }
 
-    // Maakt ascii docs en PlantUML docs aan in target/spring-modulith-docs
     @Test
     void generateDocsAndDiagrams() {
         new Documenter(modules)
                 .writeDocumentation();
 
-        // Gebruik umlStijl als je de optionele onboarding.md niet hebt gevolgd
-        var c4Stijl = Documenter.DiagramOptions.DiagramStyle.C4;
-        var umlStijl = Documenter.DiagramOptions.DiagramStyle.UML;
-
         new Documenter(modules)
                 .writeModulesAsPlantUml(Documenter.DiagramOptions.defaults()
-                        .withStyle(c4Stijl));
+                        .withStyle(Documenter.DiagramOptions.DiagramStyle.UML));
     }
 }
